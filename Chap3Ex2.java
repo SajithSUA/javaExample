70142d92285e1f417ec719b65534fde80080d2d2
sajithSUA <001sajith@gmail.com>
2020-04-09 16:51:55 +0530

    First commit

---
+++ Chap3Ex2.java
@@ -0,0 +1,24 @@
+// Compute the area of a circle. 
+class Chap3Ex2 
+{   
+  public static void main(String args[]) {     
+    double pi, r, a; 
+ 
+    r = 10.8; // radius of circle     
+    pi = 3.1416; // pi, approximately 
+    a = pi * r * r; // compute area 
+ 
+   System.out.println("Area of circle is " + a);  
+
+   int x = 123_456_789;
+  System.out.println(x); 
+  x = 123___456___789;
+  System.out.println(x); 
+  x = 0b1101_0101_0001_1010; 
+  System.out.println(x); 
+
+  float y= 6.022E2f;
+  float z= 6.0f;
+  System.out.println(y+z); 
+ } 
+} 
